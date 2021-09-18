27fe575611560277597878a14faa3f63745cdafd
Kyle Javier <kjavier@student.cscc.edu>
2021-09-18 06:49:48 -0400

    legit first commit

---
+++ awesome/autorun.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep -f $1 ;
+  then
+    $@&
+  fi
+}
+
+run picom
+run nitrogen --restore
+run volumeicon
+run lxsession
+run xfce4-volumed-pulse
+run /usr/lib/gsd-datetime
+run /usr/lib/baloo_file
+run xfce4-clipman
+run emojione-picker
+run /usr/lib/geoclue-2.0/demos/agent
+run /usr/lib/kdeconnectd
+run kdeconnect-indicator
+run nm-applet
+run system-config-printer-applet
+run start-pulseaudio-x11
+run /bin/snap userd --autostart
+run /usr/lib/tracker-miner-fs-3
+run /usr/lib/tracker-miner-rss-3
+run /usr/lib/xapps/sn-watcher/xapp-sn-watcher
+run /usr/lib/at-spi-bus-launcher --launch-immediately
+run /usr/bin/gnome-keyring-daemon --start --components=pkcs11
+run /usr/lib/gsd-power
+run /usr/bin/gnome-keyring-daemon --start --components=secrets
+run /usr/bin/gnome-keyring-daemon --start --components=ssh
+run /home/kylert/.config/awesome/keymapper.sh
---
+++ awesome/keymapper.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+key-mapper-control --command stop-all && key-mapper-control --command autoload

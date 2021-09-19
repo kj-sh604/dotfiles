d683f5256fbb57b8126728b4d86850ccd8af6fd9
Kyle Javier <kjavier@student.cscc.edu>
2021-09-19 04:13:47 -0400

    updated settings and added xfce4-power-manager on startup

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -33,3 +33,4 @@ run /usr/bin/gnome-keyring-daemon --start --components=secrets
 run /usr/bin/gnome-keyring-daemon --start --components=ssh
 run /home/kylert/.config/awesome/keymapper.sh
 run /home/kylert/.config/awesome/xinput.sh
+run xfce4-power-manager

502ba98dfc8904713ede2b83238c4c4e524c401d
Kyle Javier <kjavier@student.cscc.edu>
2021-09-20 02:52:57 -0400

    removed mouse bindings and fixed the "edit config" entry

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -88,7 +88,7 @@ awful.layout.layouts = {
 myawesomemenu = {
    { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
 --   { "manual", terminal .. " -e man awesome" },
-   { "edit config", editor_cmd .. " " .. awesome.conffile },
+   { "edit config", editor .. " " .. awesome.conffile },
 --   { "restart", awesome.restart },
 --   { "quit", function() awesome.quit() end },
 --   { "quit", function () awful.spawn("sh -c 'pkill -9 -u $USER'") end },

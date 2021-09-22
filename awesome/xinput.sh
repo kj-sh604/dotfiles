b92660117b5d68751b59a84f594d72cea3cd6143
Kyle Javier <kjavier@student.cscc.edu>
2021-09-21 20:25:03 -0400

    re-enabled natural scrolling for Logitech trackball mouse

--- awesome/xinput.sh
+++ awesome/xinput.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 sleep 2
-xinput set-prop pointer:"Logitech USB Trackball" "libinput Natural Scrolling Enabled" 0
+xinput set-prop pointer:"Logitech USB Trackball" "libinput Natural Scrolling Enabled" 1
 xinput set-prop pointer:"Logitech USB Trackball" "libinput Accel Speed" 0.90000

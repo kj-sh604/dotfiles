6d5c197025f7327ba4a6694d8b9c13e36b9938b6
Kyle Javier <kjavier@student.cscc.edu>
2021-09-23 23:12:17 -0400

    added proper (hopefully) suspend/resume notifications for the naughty daemon

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -263,6 +263,13 @@ globalkeys = gears.table.join(
     awful.key({ modkey,   "Control"}, "Down", function () awful.client.swap.byidx( -1)    end,
               {description = "swap with previous client by index", group = "client"}),
 
+    -- Notification Manipulation
+    awful.key({ modkey,          }, "n", function () naughty.suspend() end,
+              {description = "Suspend Notifications", group = "awesome"}),
+    awful.key({ modkey,          }, "m", function () naughty.resume() naughty.destroy_all_notifications() end,
+              {description = "Resume Notifications", group = "awesome"}),
+
+
       -- Unmodified Keys - Kyle
 
   --[[  awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end,

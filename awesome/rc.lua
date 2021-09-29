39369cee08307d0a50a2ccd3bc052352726a303e
Kyle Javier <kjavier@student.cscc.edu>
2021-09-29 14:10:47 -0400

    new window rules

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -582,6 +582,7 @@ awful.rules.rules = {
         -- and the name shown there might not match defined rules here.
         name = {
           "Event Tester",  -- xev.
+          "Task Manager"
         },
         role = {
           "AlarmWindow",  -- Thunderbird's calendar.

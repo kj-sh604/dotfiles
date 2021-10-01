85440f41dad706a242f19f60b21ce7fc7f4e733e
Kyle Javier <kjavier@student.cscc.edu>
2021-10-01 04:15:01 -0400

    added scroll lock support

--- awesome/keymapper.sh
+++ awesome/keymapper.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-key-mapper-control --command stop-all && key-mapper-control --command autoload && sleep 2 && setxkbmap -option compose:ralt
+key-mapper-control --command stop-all && key-mapper-control --command autoload && sleep 2 && setxkbmap -option compose:ralt && sleep 1 && xmodmap ~/.Xmodmap

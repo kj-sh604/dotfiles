24faf59f1756939aa56723ba0e2cb604648ff45c
Kyle Javier <kjavier@student.cscc.edu>
2021-10-02 02:37:58 -0400

    syntax correction for POSIX-compliance

--- awesome/xinput-daemon.sh
+++ awesome/xinput-daemon.sh
@@ -3,4 +3,4 @@
 xinput set-prop pointer:"Logitech USB Trackball" "libinput Natural Scrolling Enabled" 1
 xinput set-prop pointer:"Logitech USB Trackball" "libinput Accel Speed" 0.90000
 
-while true; do state=$(lsusb) && sleep 2 && [ '$state != $(lsusb)' ] && /home/kylert/.config/awesome/xinput.sh; done
+while true; do state=$(lsusb) && sleep 2 && [ "$state != $(lsusb)" ] && /home/kylert/.config/awesome/xinput.sh; done

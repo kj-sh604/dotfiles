40055cb02846541c9b1c33548c2cf03f86c5d644
Kyle Javier <kjavier@student.cscc.edu>
2021-10-03 18:59:34 -0400

    commented out unecessary file trackers on autostart

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -13,7 +13,7 @@ run volumeicon
 run lxsession
 run xfce4-volumed-pulse
 run /usr/lib/gsd-datetime
-run /usr/lib/baloo_file
+# run /usr/lib/baloo_file
 run xfce4-clipman
 run emojione-picker
 run /usr/lib/geoclue-2.0/demos/agent
@@ -23,8 +23,8 @@ run nm-applet
 run system-config-printer-applet
 run start-pulseaudio-x11
 run /bin/snap userd --autostart
-run /usr/lib/tracker-miner-fs-3
-run /usr/lib/tracker-miner-rss-3
+# run /usr/lib/tracker-miner-fs-3
+# run /usr/lib/tracker-miner-rss-3
 run /usr/lib/xapps/sn-watcher/xapp-sn-watcher
 run /usr/lib/at-spi-bus-launcher --launch-immediately
 run /usr/bin/gnome-keyring-daemon --start --components=pkcs11

3004df4ecaa68df0271627fe5ebbeb89fdc62e8d
Blista Kanjo <43.splash@gmail.com>
2021-10-05 15:05:05 -0400

    added PF_INFO env variables

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     
     alias ls="lsd --group-dirs first -h --icon-theme unicode -L"
+    export PF_INFO="ascii title os kernel uptime pkgs memory"
 
     
 end

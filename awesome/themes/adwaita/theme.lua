a0ed3a15dbb214ad780120b278446d5576ccf6bc
Blista Kanjo <43.splash@gmail.com>
2021-10-05 14:01:54 -0400

    modified adwaita theme with default #303030 wallpaper

--- awesome/themes/adwaita/theme.lua
+++ awesome/themes/adwaita/theme.lua
@@ -98,7 +98,7 @@ theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/m
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
 -- theme.wallpaper = themes_path.."default/background.png"
-theme.wallpaper = "/home/kylert/.config/awesome/themes/adwaita/adwaita-night.jpg"
+theme.wallpaper = "/home/kylert/.config/awesome/themes/adwaita/30times3.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"

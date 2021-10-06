99c2ae0c526a99949f2674fb35bc19069e0ad9a6
Blista Kanjo <43.splash@gmail.com>
2021-10-06 03:07:27 -0400

    changed to-do aliases for easier to reach keys

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     
     alias ls="lsd --group-dirs first -h --icon-theme unicode -L"
-    alias td="cat to-do"
-    alias etd="vim to-do"
+    alias qw="cat to-do"
+    alias eqw="vim to-do"
     export PF_INFO="ascii title os kernel uptime pkgs memory"
 
     

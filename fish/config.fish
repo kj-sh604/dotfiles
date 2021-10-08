0bc68abe2ef1b7bca3352639da8b29e2da40795b
Blista Kanjo <43.splash@gmail.com>
2021-10-08 05:20:05 -0400

    new alias for showing a preview of webcam

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@ if status is-interactive
     alias ls="lsd --group-dirs first -h --icon-theme unicode -L"
     alias qw="cat to-do"
     alias eqw="vim to-do"
+    alias sf="mplayer tv:// -tv driver=v4l2:device=/dev/video0:width=1280:height=720:fps=30:outfmt=yuy2"
     export PF_INFO="ascii title os kernel uptime pkgs memory"
 
     

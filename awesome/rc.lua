f7d5d0760a39bba534ace219a8a12e1c28618338
Blista Kanjo <43.splash@gmail.com>
2021-10-12 14:27:29 -0400

    added "zoom" to floating clients

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -563,7 +563,8 @@ awful.rules.rules = {
           "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
           "Wpa_gui",
           "veromix",
-          "xtightvncviewer"},
+          "xtightvncviewer",
+          "zoom"},
 
         -- Note that the name property shown in xprop might be set slightly after creation of the client
         -- and the name shown there might not match defined rules here.

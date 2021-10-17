1a082de3d3a89c0ddb07fd8967a8c74a6531ad3b
Blista Kanjo <43.splash@gmail.com>
2021-10-17 01:41:06 -0400

    added new mouse to xinput scripts

--- awesome/xinput-daemon.sh
+++ awesome/xinput-daemon.sh
@@ -3,5 +3,6 @@
 xinput set-prop pointer:"Logitech USB Trackball" "libinput Natural Scrolling Enabled" 1
 xinput set-prop pointer:"Logitech USB Trackball" "libinput Accel Speed" 0.90000
 xinput set-prop pointer:"SteelSeries SteelSeries Rival 310 eSports Mouse" "libinput Scrolling Pixel Distance" 30
+xinput set-prop pointer:"SteelSeries SteelSeries Rival 310 eSports Mouse" "libinput Accel Speed" 0.300000
 
 while true; do state=$(lsusb) && sleep 2 && [ "$state != $(lsusb)" ] && /home/kylert/.config/awesome/xinput.sh; done
--- awesome/xinput.sh
+++ awesome/xinput.sh
@@ -4,3 +4,4 @@ sleep 2
 xinput set-prop pointer:"Logitech USB Trackball" "libinput Natural Scrolling Enabled" 1
 xinput set-prop pointer:"Logitech USB Trackball" "libinput Accel Speed" 0.90000
 xinput set-prop pointer:"SteelSeries SteelSeries Rival 310 eSports Mouse" "libinput Scrolling Pixel Distance" 30
+xinput set-prop pointer:"SteelSeries SteelSeries Rival 310 eSports Mouse" "libinput Accel Speed" 0.300000

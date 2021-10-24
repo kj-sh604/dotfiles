b3b6d680ac64ce590199a248af1b3f343b6e85b1
Blista Kanjo <43.splash@gmail.com>
2021-10-24 17:59:31 -0400

    new move keybind

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -519,11 +519,11 @@ clientbuttons = gears.table.join(
   awful.button({ }, 1, function (c)
     c:emit_signal("request::activate", "mouse_click", {raise = true})
   end),
-  awful.button({ modkey }, 1, function (c)
+  awful.button({ modkey, "Mod1" }, 1, function (c)
     c:emit_signal("request::activate", "mouse_click", {raise = true})
     awful.mouse.client.move(c)
   end),
-  awful.button({ modkey }, 3, function (c)
+  awful.button({ modkey }, 1, function (c)
     c:emit_signal("request::activate", "mouse_click", {raise = true})
     awful.mouse.client.resize(c)
   end)

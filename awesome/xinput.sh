edb102c69722c5786f51bad5560f040988374f6c
Blista Kanjo <43.splash@gmail.com>
2021-11-04 04:29:22 -0400

    changed some peripheral configurations

--- awesome/xinput.sh
+++ awesome/xinput.sh
@@ -5,7 +5,7 @@ xinput set-prop pointer:"Logitech USB Trackball" "libinput Natural Scrolling Ena
 xinput set-prop pointer:"Logitech USB Trackball" "libinput Accel Speed" 0.90000
 xinput set-prop pointer:"SteelSeries SteelSeries Rival 310 eSports Mouse" "libinput Scrolling Pixel Distance" 50
 xinput set-prop pointer:"SteelSeries SteelSeries Rival 310 eSports Mouse" "libinput Accel Speed" 0.300000
-xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" -0.650000
+xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.300000
 xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Scrolling Pixel Distance" 50
 xinput set-prop pointer:"Logitech M705" "libinput Accel Speed" 1.000000
 xinput set-prop pointer:"Logitech M705" "libinput Scrolling Pixel Distance" 50

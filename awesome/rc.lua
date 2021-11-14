a3eb721ee72be19f8de2e4cdcc82a0d0b84f1074
Blista Kanjo <43.splash@gmail.com>
2021-11-14 14:40:46 -0500

    changed spawn method for autorun.sh

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -641,4 +641,4 @@ gears.timer {
 
 -- awful.spawn.with_shell("")
 -- awful.spawn.easy_async_with_shell("")
-awful.spawn.easy_async_with_shell("/home/kylert/.config/awesome/autorun.sh")
+awful.spawn("/home/kylert/.config/awesome/autorun.sh")

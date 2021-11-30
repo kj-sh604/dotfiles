fa588d5b688c29f18f96ca0dcce382782f633d9b
Blista Kanjo <43.splash@gmail.com>
2021-11-30 01:12:03 -0500

    removed shifted caps lock option

--- awesome/keymapper.sh
+++ awesome/keymapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-key-mapper-control --command stop-all && key-mapper-control --command autoload && sleep 1 && setxkbmap -option compose:ralt && setxkbmap -option caps:escape_shifted_capslock && xmodmap ~/.Xmodmap && xset r rate 300 50
+key-mapper-control --command stop-all && key-mapper-control --command autoload && sleep 1 && setxkbmap -option compose:ralt && xmodmap ~/.Xmodmap && xset r rate 300 50 # && setxkbmap -option caps:escape_shifted_capslock

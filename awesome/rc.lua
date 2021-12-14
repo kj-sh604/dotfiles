2fa6a254930174ee2998b7804bcdf1ccb3f1254f
Blista Kanjo <43.splash@gmail.com>
2021-12-14 14:11:09 -0500

    fixed the 'move to master' function

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -413,7 +413,7 @@ clientkeys = gears.table.join(
 
   -- _____________________________________________________________
 
-  awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end,
+  awful.key({ modkey }, "Home", function (c) c:swap(awful.client.getmaster()) end,
     {description = "move to master", group = "client"}),
   awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
     {description = "move to screen", group = "client"}),

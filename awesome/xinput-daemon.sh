014e9402a9e4d87e30f02d64ba3328dbde4d6d74
Blista Kanjo <43.splash@gmail.com>
2021-12-18 00:18:03 -0500

    removed scrolling modifiers

--- awesome/xinput-daemon.sh
+++ awesome/xinput-daemon.sh
@@ -5,9 +5,7 @@ xinput set-prop pointer:"Logitech USB Trackball" "libinput Accel Speed" 0.90000
 xinput set-prop pointer:"SteelSeries SteelSeries Rival 310 eSports Mouse" "libinput Scrolling Pixel Distance" 50
 xinput set-prop pointer:"SteelSeries SteelSeries Rival 310 eSports Mouse" "libinput Accel Speed" 0.300000
 xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.300000
-xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Scrolling Pixel Distance" 50
 xinput set-prop pointer:"Logitech M705" "libinput Accel Speed" 1.000000
-xinput set-prop pointer:"Logitech M705" "libinput Scrolling Pixel Distance" 50
 
 
 while true; do state=$(lsusb) && sleep 2 && [ "$state" != "$(lsusb)" ] && /home/kylert/.config/awesome/xinput.sh; done
--- awesome/xinput.sh
+++ awesome/xinput.sh
@@ -6,6 +6,4 @@ xinput set-prop pointer:"Logitech USB Trackball" "libinput Accel Speed" 0.90000
 xinput set-prop pointer:"SteelSeries SteelSeries Rival 310 eSports Mouse" "libinput Scrolling Pixel Distance" 50
 xinput set-prop pointer:"SteelSeries SteelSeries Rival 310 eSports Mouse" "libinput Accel Speed" 0.300000
 xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.300000
-xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Scrolling Pixel Distance" 50
 xinput set-prop pointer:"Logitech M705" "libinput Accel Speed" 1.000000
-xinput set-prop pointer:"Logitech M705" "libinput Scrolling Pixel Distance" 50

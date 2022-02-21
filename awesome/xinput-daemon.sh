17bd95279fec649ef867ed202741ad006ec9f027
Blista Kanjo <43.splash@gmail.com>
2022-02-21 13:08:26 -0500

    changed logitech USB trackball speed

--- awesome/xinput-daemon.sh
+++ awesome/xinput-daemon.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 xinput set-prop pointer:"Logitech USB Trackball" "libinput Natural Scrolling Enabled" 1
-xinput set-prop pointer:"Logitech USB Trackball" "libinput Accel Speed" 0.90000
+xinput set-prop pointer:"Logitech USB Trackball" "libinput Accel Speed" 1.000000
 xinput set-prop pointer:"SteelSeries SteelSeries Rival 310 eSports Mouse" "libinput Scrolling Pixel Distance" 50
 xinput set-prop pointer:"SteelSeries SteelSeries Rival 310 eSports Mouse" "libinput Accel Speed" 0.300000
 xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.300000

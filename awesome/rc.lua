4888d824cdc117a595cbd91fec39d45980fe6bae
Blista Kanjo <43.splash@gmail.com>
2022-04-05 14:54:48 -0400

    fixed zoom meetings that are started from a browser instance

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -592,17 +592,16 @@ awful.rules.rules = {
       "Wpa_gui",
       "veromix",
       "xtightvncviewer",
-      "zoom"},
+      "zoom",
+      "[join?]*",
+      "[start?]*",
+    },
 
     -- Note that the name property shown in xprop might be set slightly after creation of the client
     -- and the name shown there might not match defined rules here.
     name = {
       "Event Tester",  -- xev.
-      "Task Manager",
-      "Zoom - Free Account",
-      "Zoom - Licensed Account",
-      "Zoom Meeting",
-      "zoom",      
+      "Task Manager",      
     },
     role = {
       "AlarmWindow",  -- Thunderbird's calendar.

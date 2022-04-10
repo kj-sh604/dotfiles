c5aceef0c7494a2463fbcc6044e971e3ffcf7f99
Blista Kanjo <43.splash@gmail.com>
2022-04-10 06:34:11 -0400

    changed selection color from #255A9B to #11427E

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -11,7 +11,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#303030" },
-	[SchemeSel] = { "#eeeeee", "#255A9B" },
+	[SchemeSel] = { "#eeeeee", "#11427E" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */

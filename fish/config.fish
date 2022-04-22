b175b1f00eb3cab70f69aad6d27398f35a636710
Blista Kanjo <43.splash@gmail.com>
2022-04-22 11:11:32 -0400

    new "simpler" fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     
     alias ls="lsd --group-dirs first -h --icon-theme unicode -L"
-    alias qw="cat to-do"
-    alias eqw="vim to-do"
-    alias sf="mplayer tv:// -tv driver=v4l2:device=/dev/video0:width=1280:height=720:fps=30:outfmt=yuy2"
     alias s="cd ~/.local/share/scripts && lsd --group-dirs first -h --icon-theme unicode -L"
     export PF_INFO="ascii title os kernel uptime pkgs memory"
 

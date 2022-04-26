a54e658781889bb688ac649200774c09f41de37b
Blista Kanjo <43.splash@gmail.com>
2022-04-26 10:43:19 -0400

    reimplementation of mouse settings daemon

--- awesome/xinput.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sleep 2
-xinput set-prop pointer:"Logitech USB Trackball" "libinput Natural Scrolling Enabled" 1
-xinput set-prop pointer:"Logitech USB Trackball" "libinput Accel Speed" 1.000000
-xinput set-prop pointer:"SteelSeries SteelSeries Rival 310 eSports Mouse" "libinput Scrolling Pixel Distance" 50
-xinput set-prop pointer:"SteelSeries SteelSeries Rival 310 eSports Mouse" "libinput Accel Speed" 0.300000
-xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.300000
-xinput set-prop pointer:"Logitech M705" "libinput Accel Speed" 1.000000

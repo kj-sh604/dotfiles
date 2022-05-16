71c38be00340f892128241f0b40276bb35e1f339
Blista Kanjo <43.splash@gmail.com>
2022-05-16 01:43:13 -0400

    added on-the-fly window gaps keybinds

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -315,7 +315,7 @@ globalkeys = gears.table.join(
   awful.key({ modkey, "Mod1" }, "Delete", function () awful.spawn.easy_async_with_shell("xkill") end,
     {description = "kill a window by brute force", group = "launcher"}),
   awful.key({ "Control", "Mod1"          }, "Delete", function () awful.spawn("alacritty -t 'Task Manager' -e 'htop'") end,
-    {description = "Launch HTOP", group = "launcher"}),
+    {description = "launch HTOP", group = "launcher"}),
 
   -- Brightness Hotkeys
   awful.key({ }, "XF86MonBrightnessDown", function () awful.spawn.easy_async_with_shell("xbacklight -dec 15") end),
@@ -323,11 +323,22 @@ globalkeys = gears.table.join(
 
   -- Emoji Picker
   awful.key({ modkey }, "q", function () awful.spawn.easy_async_with_shell("sh -c '/home/kylert/.local/share/Blista-Kanjo-Emoji/blista-emoji-picker'") end,
-    {description = "Launch Emoji Chooser", group = "launcher"}),
+    {description = "launch Emoji Chooser", group = "launcher"}),
 
   -- Clipboard Manager
   awful.key({ modkey }, "grave", function () awful.spawn.easy_async_with_shell("xfce4-clipman-history") end,
                 {description = "open clipboard history", group = "launcher"}),
+  
+  -- On-the-fly Window Gaps configuration
+  
+  awful.key({modkey}, "'", function () awful.tag.incgap(2)   end,
+                {description = "increase window gaps", group = "window gaps"}),
+                
+  awful.key({modkey}, ";", function () awful.tag.incgap(-2)   end,
+                {description = "decrease window gaps", group = "window gaps"}),
+                
+  awful.key({modkey}, "backslash", function () awful.tag.setgap(5)   end,
+                {description = "reset window gaps", group = "window gaps"}),    -- Warning: awful.tag.setgap() is deprecated
 
   -- awesome window manager Controls
   awful.key({ "Control", "Mod1" }, "BackSpace", awesome.restart,

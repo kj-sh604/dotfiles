7d073b9651741cc46af1bd327bbcf82b7ce86dcf
Blista Kanjo <43.splash@gmail.com>
2022-05-31 14:38:02 -0400

    new script: single word spell check binded to Super+W

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -322,8 +322,12 @@ globalkeys = gears.table.join(
   awful.key({ }, "XF86MonBrightnessUp", function () awful.spawn.easy_async_with_shell("xbacklight -inc 15") end),
 
   -- Emoji Picker
-  awful.key({ modkey }, "q", function () awful.spawn.easy_async_with_shell("sh -c '/home/kylert/.local/share/Blista-Kanjo-Emoji/blista-emoji-picker'") end,
-    {description = "launch Emoji Chooser", group = "launcher"}),
+  awful.key({ modkey }, "q", function () awful.spawn.easy_async_with_shell("sh -c '~/.local/share/Blista-Kanjo-Emoji/blista-emoji-picker'") end,
+    {description = "launch emoji chooser", group = "launcher"}),
+    
+  -- Spell Checker (Single Word
+  awful.key({ modkey }, "w", function () awful.spawn.easy_async_with_shell("sh -c '~/.local/share/scripts/dym.sh'") end,
+    {description = "launch single word spell checker", group = "launcher"}),
 
   -- Clipboard Manager
   awful.key({ modkey }, "grave", function () awful.spawn.easy_async_with_shell("xfce4-clipman-history") end,
@@ -669,7 +673,7 @@ gears.timer {
 
 -- awful.spawn.with_shell("")
 -- awful.spawn.easy_async_with_shell("")
-awful.spawn.easy_async_with_shell("/home/kylert/.config/awesome/autorun.sh")
+awful.spawn.easy_async_with_shell("~/.config/awesome/autorun.sh")
 
 -- Client Swallowing Function
 

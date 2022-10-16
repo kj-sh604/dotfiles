8487b27f3c9eed155364d3d75eeb2e162f4f23ca
Blista Kanjo <43.splash@gmail.com>
2022-10-16 13:54:22 -0400

    refactor: decreased dmenu-dunst font size

--- .config/dunst/dmenu-dunst/config.def.h
+++ .config/dunst/dmenu-dunst/config.def.h
@@ -4,8 +4,8 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrains Mono:size=32",
-    "Noto Color Emoji:size=32"
+	"JetBrains Mono:size=20",
+    "Noto Color Emoji:size=20"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {

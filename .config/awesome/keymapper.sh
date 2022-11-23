8490f1620483c8135798e1ddfddfc9ce828a9407
Blista Kanjo <43.splash@gmail.com>
2022-11-23 09:00:39 -0500

    refactor: reverted back to old keymapper.sh

--- .config/awesome/keymapper.sh
+++ .config/awesome/keymapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-input-remapper-control --command stop-all && input-remapper-control --command autoload
+input-remapper-control --command stop-all && input-remapper-control --command autoload && sleep 1 && setxkbmap -option compose:ralt && sleep 1 && xmodmap ~/.Xmodmap && sleep 1 && xset r rate 300 50

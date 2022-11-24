f3c32b761f3445369d2bbbc5610568acdb8e301a
Blista Kanjo <43.splash@gmail.com>
2022-11-24 01:18:51 -0500

    refactor: changed the position of the sleep command

--- .config/awesome/keymapper.sh
+++ .config/awesome/keymapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-input-remapper-control --command stop-all && sleep 1 && input-remapper-control --command autoload
+sleep 1 && input-remapper-control --command stop-all && input-remapper-control --command autoload

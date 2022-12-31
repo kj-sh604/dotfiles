ac97ebba02f3a3dc985bd469b9c15d98e286372c
Blista Kanjo <43.splash@gmail.com>
2022-12-30 19:39:38 -0500

    feat: added reference comments for my different set-ups

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -189,7 +189,9 @@ awful.screen.connect_for_each_screen(function(s)
   }
 
   -- Create the wibox
-  s.mywibox = awful.wibar({ position = "top", screen = s, height = 22 })
+  s.mywibox = awful.wibar({ position = "top", screen = s, height = 20 })
+  -- use `height = 20` for 80 dpi
+  -- use `height = 22` for 96 dpi
 
   -- Add widgets to the wibox
   s.mywibox:setup {

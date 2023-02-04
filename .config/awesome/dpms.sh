84da4e246d8d2d644bca757e757b5566bbea74ea
Blista Kanjo <43.splash@gmail.com>
2023-02-04 08:53:20 -0500

    refactor: updated default dpms config

--- .config/awesome/dpms.sh
+++ .config/awesome/dpms.sh
@@ -5,7 +5,7 @@ if command -v xset > /dev/null; then
   # Place all DPMS settings that you want 
   # to run on awesome-wm startup below:
   xset -dpms
-  xset s off
+  xset s off off
 else
   notify-send "Error: DPMS command not found."
 fi

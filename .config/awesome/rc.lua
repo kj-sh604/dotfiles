a3a103407eae4fc46e1151d382be2ee23308b3fc
Blista Kanjo <43.splash@gmail.com>
2023-02-05 15:30:12 -0500

    feat: placing all dunstctl commands in a new submenu

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -97,18 +97,22 @@ dpms_menu = {
   { "21600 # 6h", function() awful.spawn.easy_async_with_shell("xset dpms 0 0 21600") end },
 }
 
+dunst_menu = {
+  { "history-pop", function() awful.spawn.easy_async_with_shell("dunstctl history-pop") end },
+  { "set-paused true", function() awful.spawn.easy_async_with_shell("dunstctl set-paused true") end },
+  { "set-paused false", function() awful.spawn.easy_async_with_shell("dunstctl set-paused false") end },
+}
+
 myawesomemenu = {
   { "show hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
   --   { "manual", terminal .. " -e man awesome" },
   { "xset s", s_menu },
   { "xset dpms", dpms_menu },
+  { "dunstctl", dunst_menu },
   { "config file", editor .. " " .. awesome.conffile },
   { "picom config", function() awful.spawn.easy_async_with_shell("sh -c 'gvim  $HOME/.config/picom.conf'") end },
   { "change wallpaper", function() awful.spawn.easy_async_with_shell("sh -c 'nitrogen'") end },
   { "xdg_menu refresh", function() awful.spawn.easy_async_with_shell("sh -c 'xdg_menu --format awesome --root-menu /etc/xdg/menus/arch-applications.menu > ~/.config/awesome/xdgmenu.lua'") end },
-  { "dunst_hist", function() awful.spawn.easy_async_with_shell("dunstctl history-pop") end },
-  { "dunst=0", function() awful.spawn.easy_async_with_shell("dunstctl set-paused true") end },
-  { "dunst=1", function() awful.spawn.easy_async_with_shell("dunstctl set-paused false") end },
   { "refresh", awesome.restart },
   { "reboot", function() awful.spawn("sh -c 'lxsudo reboot now'") end },
   --   { "quit", function() awesome.quit() end },

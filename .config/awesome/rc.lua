456af514501dd26a8f635776651985dff573014b
Blista Kanjo <43.splash@gmail.com>
2023-05-21 16:36:21 -0400

    refactor: whitespace fix

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -486,7 +486,7 @@ globalkeys = gears.table.join(
 )
 
 clientkeys = gears.table.join(
-  awful.key({ modkey,           }, "f",
+  awful.key({ modkey }, "f",
     function (c)
       c.fullscreen = not c.fullscreen
       c:raise()

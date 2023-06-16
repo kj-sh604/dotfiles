96e6bbeb73821f8f88802317fc4fa8af7a88fa25
Blista Kanjo <43.splash@gmail.com>
2023-06-15 22:29:23 -0400

    refactor: cleaner xinput-daemon

--- .config/awesome/xinput-daemon.sh
+++ .config/awesome/xinput-daemon.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
 
-
-apply_settings () {
-
+# Function to apply the desired settings
+apply_settings() {
 	sleep 2
 	xinput set-prop pointer:"Logitech USB Trackball" "libinput Natural Scrolling Enabled" 1
 	xinput set-prop pointer:"Logitech USB Trackball" "libinput Accel Speed" 1.000000
 	xinput set-prop pointer:"SteelSeries SteelSeries Rival 310 eSports Mouse" "libinput Accel Speed" 0.300000
 	xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.300000
 	xinput set-prop pointer:"Logitech M705" "libinput Accel Speed" 1.000000
-
 }
 
+# Initial application of settings
 apply_settings
 
-while true; do state=$(lsusb) && sleep 2 && [ "$state" != "$(lsusb)" ] && apply_settings; done
+# Continuously monitor for changes in USB devices and reapply settings if any change is detected
+while true; do
+	# Get the current state of USB devices
+	state=$(lsusb)
+
+	# Wait for 2 seconds
+	sleep 2
+
+	# Compare the current state with the new state of USB devices
+	# If any change is detected, reapply the settings
+	if [ "$state" != "$(lsusb)" ]; then
+		apply_settings
+	fi
+done

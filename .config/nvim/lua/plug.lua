8218d7b4f37037c205d3b29ce972cff0ece01af5
Blista Kanjo <43.splash@gmail.com>
2023-06-23 03:53:00 -0400

    refactor: comment structure for plug.lua

--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -1,8 +1,7 @@
--- Plugin manager (vim-plug)
+-- legacy plugin manager config (vim-plug)
 vim.cmd [[
   call plug#begin()
   Plug 'https://github.com/preservim/nerdtree', { 'on': 'NERDTreeToggle' }
-  " Plug 'LunarWatcher/auto-pairs'
   Plug 'tmsvg/pear-tree'
   Plug 'https://github.com/adelarsq/vim-matchit'
   Plug 'neoclide/coc.nvim', {'branch': 'release'}
@@ -10,6 +9,8 @@ vim.cmd [[
   Plug 'sbdchd/neoformat'
   Plug 'ThePrimeagen/vim-be-good'
   Plug 'junegunn/fzf'
+
+  " Plug 'LunarWatcher/auto-pairs'
   call plug#end()
 ]]
 

5988c0fd8d16aba8dbe995ef349ba3883c4c012c
Blista Kanjo <43.splash@gmail.com>
2023-06-23 04:16:56 -0400

    refactor: comments case

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -22,7 +22,7 @@ local opts = {
     ttyfast = true,
 }
 
--- Customize split dividers
+-- customize split dividers
 vim.opt.fillchars = vim.opt.fillchars + {
   vert = "█",
   fold = "█",
@@ -32,15 +32,15 @@ vim.opt.fillchars = vim.opt.fillchars + {
   stl = "="
 }
 
--- Use a line cursor within insert mode and a block cursor everywhere else.
--- Reference chart of values:
---   Ps = 0  -> blinking block.
---   Ps = 1  -> blinking block (default).
---   Ps = 2  -> steady block.
---   Ps = 3  -> blinking underline.
---   Ps = 4  -> steady underline.
---   Ps = 5  -> blinking bar (xterm).
---   Ps = 6  -> steady bar (xterm).
+-- use a line cursor within insert mode and a block cursor everywhere else.
+-- reference chart of values:
+--   ps = 0  -> blinking block.
+--   ps = 1  -> blinking block (default).
+--   ps = 2  -> steady block.
+--   ps = 3  -> blinking underline.
+--   ps = 4  -> steady underline.
+--   ps = 5  -> blinking bar (xterm).
+--   ps = 6  -> steady bar (xterm).
 vim.g.t_SI = "\27[6 q"
 vim.g.t_EI = "\27[2 q"
 

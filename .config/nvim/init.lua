b5dee036ef1fa2f0e50d032da2c613ac1452aa5b
Blista Kanjo <43.splash@gmail.com>
2023-06-25 03:29:33 -0400

    feat: implemented nvim spellcheck

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,3 +6,6 @@ require('opts')
 require('plug')
 require('tele')
 require('tree')
+
+-- enable spellcheck
+vim.cmd('setlocal spelllang=en_us,tl') -- english(us) and filipino(tagalog)
--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -17,3 +17,6 @@ vim.api.nvim_set_keymap("n", "<leader>n", ":NERDTreeFocus<CR>", { noremap = true
 vim.api.nvim_set_keymap("n", "<C-n>", ":NERDTree<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-t>", ":NERDTreeToggle<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>/", ":NERDTreeFind<CR>", { noremap = true })
+
+-- spell check keybinds
+vim.api.nvim_set_keymap("n", "<leader>sc", ":set spell!<CR>", { silent = true })

afbd156fcc9917ae46620986d5f56ca534082193
Blista Kanjo <43.splash@gmail.com>
2023-07-12 04:33:54 -0400

    feat: `awesome 4.3.3` backward compatibility

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -37,24 +37,30 @@ editor_cmd = terminal .. " -e " .. editor
 -- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
 
--- Table of layouts to cover with awful.layout.inc, order matters.
+-- table of layouts to cover with awful.layout.inc, order matters. (-git compatible)
 tag.connect_signal("request::default_layouts", function()
     awful.layout.append_default_layouts({
-        -- awful.layout.suit.floating,
         awful.layout.suit.tile,
-        -- awful.layout.suit.tile.left,
-        -- awful.layout.suit.tile.bottom,
-        -- awful.layout.suit.tile.top,
+        awful.layout.suit.max,
+        -- awful.layout.suit.corner.nw,
         -- awful.layout.suit.fair,
         -- awful.layout.suit.fair.horizontal,
+        -- awful.layout.suit.floating,
+        -- awful.layout.suit.magnifier,
+        -- awful.layout.suit.max.fullscreen,
         -- awful.layout.suit.spiral,
         -- awful.layout.suit.spiral.dwindle,
-        awful.layout.suit.max,
-        -- awful.layout.suit.max.fullscreen,
-        -- awful.layout.suit.magnifier,
-        -- awful.layout.suit.corner.nw,
+        -- awful.layout.suit.tile.bottom,
+        -- awful.layout.suit.tile.left,
+        -- awful.layout.suit.tile.top,
     })
 end)
+
+-- active layout table (4.3.3 compatible)
+awful.layout.layouts = {
+    awful.layout.suit.tile,
+    awful.layout.suit.max,
+}
 -- }}}
 
 -- {{{ Sub-Menus

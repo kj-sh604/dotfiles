d3951980c9bcc564782038fe88854628b088943a
Blista Kanjo <43.splash@gmail.com>
2023-07-17 14:22:14 -0400

    refactor: using the `rust coreutils` again

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,10 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # commands to run in interactive sessions can go here
         fish_vi_key_bindings
 
-    # Personal Aliases
-        alias s="cd ~/.local/bin && ls --group-directories-first -h -p --color -F"
+    # personal aliases
         alias ls="ls --group-directories-first -h -p --color -F"
+        alias s="cd ~/.local/bin && ls"
         alias d="disown"
         alias c="cal"
         alias w="curl wttr.in"
@@ -20,7 +20,7 @@ if status is-interactive
         alias yt-webm="youtube-dl -cif 'bestvideo[ext=webm]+bestaudio[ext=webm]/best[ext=webm]/best'"
         alias yt-m4a="youtube-dl -cif 'bestaudio[ext=m4a]'"
 
-    # Pomodoro Timer Aliases
+    # pomodoro timer aliases
         alias work="timer 30m && notify-send \
         'Pomodoro' 'Work Timer is up! Take a Break 😊' -i \
         ~/.cache/pomo/pomo-tomato.png -t 30000 -w -A 'Dismiss'\
@@ -31,10 +31,23 @@ if status is-interactive
         ~/.cache/pomo/pomo-tomato.png -t 30000 -w -A 'Dismiss'\
         & disown; mpv '/home/kylert/.cache/pomo/pomo-sound.mp3'"
 
-    # Environment Variable Declarations
+    # rust-uutils command substitutions
+        alias ln=uu-ln
+
+        ## experimental/risky rust-uutits substitutions for testing
+        alias basenc=uu-basenc
+        alias groups=uu-groups
+        alias hostname=uu-hostname
+        alias join=uu-join
+        alias kill=uu-kill
+        alias pathchk=uu-pathchk
+        alias realpath=uu-realpath
+        alias touch=uu-touch
+
+    # environment variable declarations
         export PF_INFO="ascii title os kernel uptime pkgs memory"
 
-    # LESS TERMCAP Variables (for colored man pages)
+    # less termcap variables (for colored man pages)
         set -gx LESS_TERMCAP_mb \e'[1;32m'
         set -gx LESS_TERMCAP_md \e'[1;32m'
         set -gx LESS_TERMCAP_me \e'[0m'

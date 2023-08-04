968b2289024e92d5f591906b0e4f262d76d540b7
Blista Kanjo <43.splash@gmail.com>
2023-08-04 09:06:07 -0400

    refactor: change git untracked files color in `fish`

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -17,7 +17,7 @@ set -g __fish_git_prompt_char_cleanstate "✓"
 set -g __fish_git_prompt_color_dirtystate blue
 set -g __fish_git_prompt_color_stagedstate yellow
 set -g __fish_git_prompt_color_invalidstate red
-set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_untrackedfiles blue --bold
 set -g __fish_git_prompt_color_cleanstate green --bold
 
 function fish_prompt

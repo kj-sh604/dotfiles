98a2e3db1531f20bc2e57650344594e732654920
Blista Kanjo <43.splash@gmail.com>
2023-08-23 08:16:35 -0400

    refactor: rename `rc-stable.lua` to `rc-4.3-3.lua`

--- .config/awesome/rc-4.3-3.lua
+++ .config/awesome/rc-4.3-3.lua
@@ -667,12 +667,11 @@ for i = 1, 9 do
         -- move client to prev/next tag and switch to it
         awful.key({ modkey, "Shift" }, "Left",
             function()
-                -- get current tag
                 local t = client.focus and client.focus.first_tag or nil
                 if t == nil then
                     return
                 end
-                -- get previous tag (modulo 9 excluding 0 to wrap from 1 to 9)
+
                 local tag = client.focus.screen.tags[(t.name - 2) % 9 + 1]
                 client.focus:move_to_tag(tag)
                 awful.tag.viewprev()
@@ -680,12 +679,11 @@ for i = 1, 9 do
             { description = "move client to previous tag and switch to it", group = "tag" }),
         awful.key({ modkey, "Shift" }, "Right",
             function()
-                -- get current tag
                 local t = client.focus and client.focus.first_tag or nil
                 if t == nil then
                     return
                 end
-                -- get next tag (modulo 9 excluding 0 to wrap from 9 to 1)
+
                 local tag = client.focus.screen.tags[(t.name % 9) + 1]
                 client.focus:move_to_tag(tag)
                 awful.tag.viewnext()

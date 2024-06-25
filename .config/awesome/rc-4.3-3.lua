b5eeb77c8c6e88aff183bb82df98bcdcbc304131
Blista Kanjo <43.splash@gmail.com>
2024-06-25 13:28:51 -0400

    refactor: revert to `e165fa8`

--- .config/awesome/rc-4.3-3.lua
+++ .config/awesome/rc-4.3-3.lua
@@ -703,11 +703,11 @@ clientbuttons = gears.table.join(
     awful.button({}, 1, function(c)
         c:emit_signal("request::activate", "mouse_click", { raise = true })
     end),
-    awful.button({ modkey, "Control" }, 1, function(c)
+    awful.button({ modkey }, 1, function(c)
         c:emit_signal("request::activate", "mouse_click", { raise = true })
         awful.mouse.client.move(c)
     end),
-    awful.button({ modkey }, 1, function(c)
+    awful.button({ "Mod1" }, 1, function(c)
         c:emit_signal("request::activate", "mouse_click", { raise = true })
         awful.mouse.client.resize(c)
     end)

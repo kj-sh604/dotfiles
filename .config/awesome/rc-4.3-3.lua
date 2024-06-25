1ff0a83d36ded4d4531f40c14327d7358bb49abc
Blista Kanjo <43.splash@gmail.com>
2024-06-25 10:33:34 -0400

    refactor: `"Control"` in place of `"Mod"`

--- .config/awesome/rc-4.3-3.lua
+++ .config/awesome/rc-4.3-3.lua
@@ -703,7 +703,7 @@ clientbuttons = gears.table.join(
     awful.button({}, 1, function(c)
         c:emit_signal("request::activate", "mouse_click", { raise = true })
     end),
-    awful.button({ modkey, "Mod1" }, 1, function(c)
+    awful.button({ modkey, "Control" }, 1, function(c)
         c:emit_signal("request::activate", "mouse_click", { raise = true })
         awful.mouse.client.move(c)
     end),
--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -613,7 +613,7 @@ client.connect_signal("request::default_mousebindings", function()
         awful.button({}, 1, function(c)
             c:emit_signal("request::activate", "mouse_click", { raise = true })
         end),
-        awful.button({ modkey, "Mod1" }, 1, function(c)
+        awful.button({ modkey, "Control" }, 1, function(c)
             c:emit_signal("request::activate", "mouse_click", { raise = true })
             awful.mouse.client.move(c)
         end),

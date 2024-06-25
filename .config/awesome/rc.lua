0df0a3790c42c551546d257dbde2e5d56986e7e2
Blista Kanjo <43.splash@gmail.com>
2024-06-25 10:28:50 -0400

    refactor: change resize & move keybinds

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -613,11 +613,11 @@ client.connect_signal("request::default_mousebindings", function()
         awful.button({}, 1, function(c)
             c:emit_signal("request::activate", "mouse_click", { raise = true })
         end),
-        awful.button({ modkey }, 1, function(c)
+        awful.button({ modkey, "Mod1" }, 1, function(c)
             c:emit_signal("request::activate", "mouse_click", { raise = true })
             awful.mouse.client.move(c)
         end),
-        awful.button({ modkey, "Mod1" }, 1, function(c)
+        awful.button({ modkey }, 1, function(c)
             c:emit_signal("request::activate", "mouse_click", { raise = true })
             awful.mouse.client.resize(c)
         end)

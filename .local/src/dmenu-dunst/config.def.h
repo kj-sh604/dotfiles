24c96da496aa1e6b8e087a45fd621da39dac8bfd
kj-sh604 <43.splash@gmail.com>
2024-08-10 00:00:07 -0400

    refactor: revert to default value for `menu_height_ratio`

--- .local/src/dmenu-dunst/config.def.h
+++ .local/src/dmenu-dunst/config.def.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 250;                    /* minimum width when centered */
-static const float menu_height_ratio = 2.0f;  /* This is the ratio used in the original calculation */
+static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"JetBrains Mono:size=14",

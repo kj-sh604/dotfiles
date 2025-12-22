ead5248907ae835a9f50c051df2bf91e0b0cd939
kj_sh604 <43.splash@gmail.com>
2025-12-22 01:15:12 -0500

    feat: attempt fish style pwd in zsh

---
+++ .config/shell/git-prompts/kj_sh604-with-attempt-at-fish-style-pwd.zsh
@@ -0,0 +1,21 @@
+ZSH_GIT_PROMPT_FORCE_BLANK=1
+ZSH_GIT_PROMPT_SHOW_UPSTREAM="yes"
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_no_bold[white]%}("
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg_no_bold[white]%}) "
+ZSH_THEME_GIT_PROMPT_SEPARATOR=" "
+ZSH_THEME_GIT_PROMPT_DETACHED="%{$fg_no_bold[cyan]%}:"
+ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_no_bold[magenta]%}"
+ZSH_THEME_GIT_PROMPT_UPSTREAM_SYMBOL="%{$fg_bold[yellow]%}^ "
+ZSH_THEME_GIT_PROMPT_UPSTREAM_PREFIX="%{$fg[red]%}(%{$fg[yellow]%}"
+ZSH_THEME_GIT_PROMPT_UPSTREAM_SUFFIX="%{$fg[red]%})"
+ZSH_THEME_GIT_PROMPT_BEHIND="%{$fg_no_bold[white]%}↓"
+ZSH_THEME_GIT_PROMPT_AHEAD="%{$fg_no_bold[white]%}↑"
+ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg[red]%}x"
+ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[yellow]%}•"
+ZSH_THEME_GIT_PROMPT_UNSTAGED="%{$fg[blue]%}+"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[blue]%}U"
+ZSH_THEME_GIT_PROMPT_STASHED="%{$fg[blue]%}☐"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}✓"
+
+PROMPT=$'%F{cyan}${${${:-$(printf "/%.1s" ${(s./.)PWD:h})/${PWD:t}}/\\/\\///}//\\%/%%}%f %F{242}$(gitprompt)%f%(12V.%F{242}%12v%f .)%(?.%F{white}.%F{white})%%%f '
